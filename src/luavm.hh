b055075390a8b76c0655d4adb660be02915f8491
IkerGalardi <contacto.ikergalardi@gmail.com>
2020-08-31 10:17:54 +0200

    Added error checking functions

--- src/luavm.hh
+++ src/luavm.hh
@@ -13,7 +13,10 @@ public:
 
     void add_native_function(luafunc func, const char* name);
     bool execute_script(const std::string& script);
+
+    const std::string& get_last_error();
 private:
     lua_State* vm;
+    std::string last_error;
 protected:
 };
