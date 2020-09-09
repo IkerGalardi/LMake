8bb265811aadd1f8ef285fd1a9f722222fa759df
IkerGalardi <contacto.ikergalardi@gmail.com>
2020-09-09 09:47:18 +0200

    Implementation of last error

--- src/luavm.cc
+++ src/luavm.cc
@@ -16,5 +16,10 @@ void luavm::add_native_function(luafunc func, const char* name) {
 bool luavm::execute_script(const char* script) {
     bool res = luaL_dostring(vm, script);
     // TODO: get error type
+    last_error = "Lua execution error";
     return res;
+}
+
+const std::string& luavm::get_last_error() {
+    return last_error;
 }
