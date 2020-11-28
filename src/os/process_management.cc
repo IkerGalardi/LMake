fff83590558419dc2619add67cbe4c96fbb4a1e9
Iker Galardi <contacto.ikergalardi@gmail.com>
2020-11-28 12:31:17 +0100

    Enhancement: Use path env variable when executing processes. (#47)
    For creating more general lmake scripts that could potentially be more cross platform and more readable, when setting the compiler, linker or executing a system process, instead of giving the full path, the user should be able to just add the program name and lmake search on the `PATH` variable.
    
    On function `run_process` there is a check if a path is there. If there the specified process is inside a path just the process is executed, if not, the executable is searched on `PATH`. This functionality is similar to what bash does on the command line.

--- src/os/process_management.cc
+++ src/os/process_management.cc
@@ -58,7 +58,13 @@ namespace os {
             std::string temp = std::string(prog) + " " + args;
             auto args = string_split_null_terminated(temp, ' ');
 
-            int err = execv(prog.c_str(), args.data());
+            int err;
+            if(prog.find("/") != std::string::npos) {
+                err = execv(prog.c_str(), args.data());
+            } else {
+                err = execvp(prog.c_str(), args.data());
+            }
+
             std::cerr << "[E] Cannot execute process\n";
             std::exit(5);
         }
