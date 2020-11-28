446d47fd8cd713eb28e11f17d6a6d2ffdef34164
IkerGalardi <contacto.ikergalardi@gmail.com>
2020-11-28 09:42:11 +0100

    compilation, find and link tests have been removed for now

--- test/test.sh
+++ test/test.sh
@@ -8,24 +8,6 @@ sh test.sh
 cd ..
 
 echo 
-echo "[+] Running find test"
-cd find
-sh test.sh
-cd ..
-
-echo 
-echo "[+] Running compilation test"
-cd compile
-sh test.sh
-cd ..
-
-echo 
-echo "[+] Running linking test"
-cd link
-sh test.sh
-cd ..
-
-echo 
 echo "[+] Running full project test"
 cd full_project
 sh test.sh
