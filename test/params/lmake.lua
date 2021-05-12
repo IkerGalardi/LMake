673e416f4929fc8eb6141efdfbd75ab25e5fe33f
Iker Galardi <contacto.ikergalardi@gmail.com>
2021-05-12 16:02:13 +0200

    Bug: crash on incorrect parameters (#96)
    Whenever a parameter is taken from the virtual machine is checked if its correct, if not an error is shown and exited. Excessive parameters don't cause any harm so they are not checked.

---
+++ test/params/lmake.lua
@@ -0,0 +1,47 @@
+function test_noparams1()
+    lmake_set_compiler()
+end
+
+function test_noparams2()
+    lmake_set_compiler_flags()
+end
+
+function test_noparams3()
+    lmake_set_compiler_out()
+end
+
+function test_noparams4()
+    lmake_compile()
+end
+
+function test_noparams5()
+    lmake_set_linker()
+end
+
+function test_noparams6()
+    lmake_set_linker_flags()
+end
+
+function test_noparams7()
+    lmake_set_linker_out()
+end
+
+function test_noparams8()
+    lmake_link()
+end
+
+function test_noparams9()
+    lmake_chdir()
+end
+
+function test_noparams10()
+    lmake_exec()
+end
+
+function test_noparams11()
+    lmake_error()
+end
+
+function test_noparams12()
+    lmake_find()
+end
